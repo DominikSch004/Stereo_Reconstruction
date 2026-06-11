af403928f4b7ec314aea7f14eaf37d0195cc3fda
Andres Forero <andres.forero@predium.de>
2026-06-11 18:06:31 +0200

    added DTU loader, imported freeimage implementations

---
+++ include/DTULoader.hpp
@@ -0,0 +1,56 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <FreeImageHelper.h>
+
+/* Assumes path is saved as /data/dtu/Rectified/scan%id%/%image%, loads an image pair to memory from
+three ids, scan_id, rightImageId and viewIdRight + an optional illumination */
+
+struct StereoPair
+{
+    FreeImageB imageLeft;
+    FreeImageB imageRight;
+}
+
+class DTULoader
+{
+public:
+    DTULoader(const std::string &datasetDir) : m_baseDir {}
+
+    StereoPair loadPair(int scanId, int viewIdLeft, int viewIdRight, int illumination = 3)
+    {
+        StereoPair pair;
+
+        std::string pathLeft = buildImagePath(scanId, viewIdLeft, illumination);
+        std::string pathRight = buildImagePath(scanId, viewIdRight, illumination);
+
+        std::cout << "Loading left image: " << pathLeft << "\n";
+        pair.imageLeft = loadToFreeImage(pathLeft);
+
+        std::cout << "Loading right image: " << pathRight << "\n";
+        pair.imageRight = loadToFreeimage(pathRight);
+
+        return pair;
+    }
+
+private:
+    std::string m_baseDir;
+
+    std::string buildImagePath(int scanId, int viewId, int illumination)
+    {
+        char viewStr[10];
+        snprintf(viewStr, sizeof(viewStr), "%03d", viewId + 1);
+        return m_baseDir + "/Rectified/scan" + std::to_string(scanId) +
+               "/rect_" + std::string(viewStr) + "_" + std::to_string(illumination) + "_r5000.png";
+    }
+
+    FreeImageB loadToFreeImage(const std::string &filepath)
+    {
+        FreeImageB fi;
+        if (!fi.LoadImageFromFile(filepath))
+        {
+            std::cerr << "ERROR: Failed to load image at " << filepath << "\n";
+        }
+        return fi;
+    }
+}
