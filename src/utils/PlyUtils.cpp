57e26163523204d4836fd72e851428502d73afb0
Francisco Mónica <francisco.monica@tum.de>
2026-07-06 17:32:15 +0200

    Fix invalid disparity check in reprojectManual to minDisp -1 #23

--- src/utils/PlyUtils.cpp
+++ src/utils/PlyUtils.cpp
@@ -49,7 +49,7 @@ PointCloud PlyUtils::buildPointCloud(
     else
         disparity.convertTo(disp32f, CV_32F);
 
-    cv::Mat pts3D = Triangulation::reprojectDisparityTo3D(disp32f, Q, P1r, P2r, method);
+    cv::Mat pts3D = Triangulation::reprojectDisparityTo3D(disp32f, Q, P1r, P2r, minDisp, method);
     if (pts3D.empty())
         return PointCloud();
 
