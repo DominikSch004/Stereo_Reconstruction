27c7c572d4a8e41368dd9bf5f2e3ae9c97861fd3
Andres Forero <andres.forero@predium.de>
2026-07-12 22:39:32 +0200

    clean up: removed freeImage

--- include/ImgUtils.hpp
+++ include/ImgUtils.hpp
@@ -3,19 +3,12 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <eigen3/Eigen/Dense>
-#include "FreeImageHelper.h"
 
 // Convert Eigen 3x3 to cv::Mat (CV_64F)
 cv::Mat toCvMat(const Eigen::Matrix3d &M);
 
-// Convert FreeImageB (RGBA) to OpenCV grayscale cv::Mat
-cv::Mat toGray(const FreeImageB &fi);
-
 // Convert cv::Mat img to gray
 cv::Mat toGray(const cv::Mat &img);
-
-// Convert FreeImage buffers to OpenCV mats
-cv::Mat toBGR(const FreeImageB &fi);
 
 // Convert 3x3 OpenCV matrix (CV_64F) to Eigen Matrix3d
 Eigen::Matrix3d toEigenMat(const cv::Mat &cvMat);
